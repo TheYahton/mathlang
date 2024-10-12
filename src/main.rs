aa50e35350e293e36bd0627cbbf0d5fb0813b114
TheYahton <>
2024-10-13 02:24:20 +0300

    main.rs less nesting

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,47 @@ use mathlang::interpreter::interpret;
 use mathlang::lexer::tokenize;
 use mathlang::parser::ast;
 
+fn meta(source: &String) {
+    println!("{}", interpret(ast(tokenize(&source))));
+}
+
 fn main() {
     println!("Mathlang 0.0.1");
     let args: Vec<_> = std::env::args().collect();
-    if args.len() > 1 {
-        if args[1] == "-i" {
-            loop {
-                print!(">>> ");
-                io::stdout().flush().unwrap();
-                let mut input_string = String::new();
-                input_string.clear();
-                io::stdin().read_line(&mut input_string).unwrap();
-                println!("{}", interpret(ast(tokenize(&input_string))));
-            }
-        } else {
-            let path = Path::new(&args[1]);
-            if !path.exists() {
-                println!("No such file or directory.");
-                return;
-            }
-            let mut file = match File::open(&path) {
-                Err(why) => panic!("{}", why),
-                Ok(file) => file,
-            };
-            let mut text = String::new();
-            match file.read_to_string(&mut text) {
-                Err(why) => panic!("{}", why),
-                Ok(_) => (),
-            }
-            println!("{}", interpret(ast(tokenize(&text))));
-        }
-    } else {
+    if args.len() < 2 {
         println!("Usage: mathlang FILE or mathlang -i \"1/0\"");
+        return;
     }
+
+    if args[1] == "-i" {
+        if args.len() > 2 {
+            meta(&args[2]);
+            return;
+        }
+        loop {
+            print!(">>> ");
+            io::stdout().flush().unwrap();
+            let mut input_string = String::new();
+            input_string.clear();
+            io::stdin().read_line(&mut input_string).unwrap();
+            meta(&input_string);
+        }
+    }
+
+    let path = Path::new(&args[1]);
+    if !path.exists() {
+        println!("No such file or directory.");
+        return;
+    }
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("{}", why),
+        Ok(file) => file,
+    };
+    let mut text = String::new();
+    match file.read_to_string(&mut text) {
+        Err(why) => panic!("{}", why),
+        Ok(_) => (),
+    }
+
+    meta(&text);
 }
